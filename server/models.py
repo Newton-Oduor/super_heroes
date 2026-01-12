36f0a8e672d7e6214a2ee01cd76bb42bb25e152b
Newton Oduor <newtonochieng@gmail.com>
2026-01-12 22:36:31 +0300

    Creates Hero tables & relationship (models.py)

--- server/models.py
+++ server/models.py
@@ -7,3 +7,13 @@ db = SQLAlchemy(metadata=metadata)
 
 
 
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
+
+    id = db.column(db.integer, primary_key=True)
+    name = db.column(db.string, nullable=False)
+    super_name = db.Column(db.String, nullable=False)
+
+    hero_powers = db.relationship('HeroPower', back_populates='hero', cascade='all, delete-orphan')
+
+    serialize_rules = ('-hero_powers.hero',)
