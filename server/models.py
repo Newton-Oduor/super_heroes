aa8107451179f9e39a5a9de42e91dbbfe833e80f
Newton Oduor <newtonochieng@gmail.com>
2026-01-12 23:29:53 +0300

    Creates database (flask alembic autogenerate)

--- server/models.py
+++ server/models.py
@@ -12,7 +12,7 @@ class Hero(db.Model, SerializerMixin):
     __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.string, nullable=False)
+    name = db.Column(db.String, nullable=False)
     super_name = db.Column(db.String, nullable=False)
 
     hero_powers = db.relationship('HeroPower', back_populates='hero', cascade='all, delete-orphan')
