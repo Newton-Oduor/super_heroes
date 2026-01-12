371a43abcb45ee29be8e4dc1a1e8218d638dd1f4
Newton Oduor <newtonochieng@gmail.com>
2026-01-12 22:42:40 +0300

    Creates Power tables & relationship (models.py)

--- server/models.py
+++ server/models.py
@@ -10,10 +10,22 @@ db = SQLAlchemy(metadata=metadata)
 class Hero(db.Model, SerializerMixin):
     __tablename__ = 'heroes'
 
-    id = db.column(db.integer, primary_key=True)
+    id = db.column(db.Integer, primary_key=True)
     name = db.column(db.string, nullable=False)
     super_name = db.Column(db.String, nullable=False)
 
     hero_powers = db.relationship('HeroPower', back_populates='hero', cascade='all, delete-orphan')
 
     serialize_rules = ('-hero_powers.hero',)
+
+
+class Power(db.Model, SerializerMixin):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+
+    hero_powers = db.relationship('HeroPower', back_populates='power', cascade='all, delete-orphan')
+
+    serialize_rules = ("-hero_powers.power",)
