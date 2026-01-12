058aeebaae4cf3713e5365532579666f15548d72
Newton Oduor <newtonochieng@gmail.com>
2026-01-12 22:22:21 +0300

    Creates the Flask app, connects it to a database, enables safe database changes, and turns it into a REST API.

--- server/app.py
+++ server/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
-from flask_restful import Api
-from flask_alchemy import SQLAlchemy
+from flask_restful import Api, Resource
 from flask_migrate import Migrate
 
 from models import db, Hero, Power, HeroPower
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db.init_app(app)
+migrate = Migrate(app, db)
+api = Api(app)
