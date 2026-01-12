c02c7be27839abefb35a1a1c17c4097e80bad519
Newton Oduor <newtonochieng@gmail.com>
2026-01-12 23:09:25 +0300

    Adds logic to GET Heroes and GET Hero by ID

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 
@@ -11,6 +11,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 api = Api(app)
+
+
+# GET all heroes
+class Heroes(Resource):
+    def get(self):
+        return [hero.to_dict() for hero in Hero.query.all()], 200
+
+# GET hero by ID
+class HeroByID(Resource):
+    def get(self, id):
+        hero = Hero.query.get(id)
+        if not hero:
+            return{'error': 'Hero not found'}, 404
+        return hero.to_dict(), 200
 
 
 if __name__ == "__main__":
